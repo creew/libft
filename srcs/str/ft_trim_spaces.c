896e69d40e01e68865f91e1d5ed129c38c42b4e1
creew <workfast@bk.ru>
2020-08-21 19:59:40 +0300

    fix trimming empty string

--- srcs/str/ft_trim_spaces.c
+++ srcs/str/ft_trim_spaces.c
@@ -14,15 +14,18 @@
 
 char	*ft_trim_spaces(char *str)
 {
-	size_t	len;
-	char	*r;
+	size_t len;
+	char * r;
 
 	r = str;
 	while (ft_isspace(*str))
 		str++;
 	len = ft_strlen(str);
-	while (ft_isspace(str[len - 1]))
-		str[--len] = '\0';
+	if (len)
+	{
+		while (ft_isspace(str[len - 1]))
+			str[--len] = '\0';
+	}
 	ft_memmove(r, str, len + 1);
 	return (r);
 }
